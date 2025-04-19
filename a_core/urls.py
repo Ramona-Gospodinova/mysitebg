36f0e7ec873e7152b8fca1a23e7385cc3b42bd90
Ramona Gospodinova <ramona.gospodinova@plana.solutions>
2025-04-19 17:13:08 +0300

    changes

--- a_core/urls.py
+++ a_core/urls.py
@@ -5,6 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),  # Регистрация и вход
-    path('', include('apps.services.urls')),
     path('', include('apps.accounts.urls')),
+    path('', include('apps.services.urls')),
 ]
--- apps/services/urls.py
+++ apps/services/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from .views import cases_view, use_case_view, use_case_by_tag_view, contacts_view
+from .views import contacts_view
 
 urlpatterns = [
-    path('use-cases/', cases_view, name='cases'),
-    path('use-cases/<pk>/', use_case_view, name='use-case-single'),
-    path('use-cases/tag/<slug:tag_slug>/', use_case_by_tag_view, name='use-cases-by-tag'),
     path('contacts/', contacts_view, name='contacts'),
 ]
